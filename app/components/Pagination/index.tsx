da75446fce1300139b77428f31bd165410a1b0fe
MSI PC <navodinith@gmail.com>
2024-06-18 22:34:09 +0530

    key property

--- app/components/Pagination/index.tsx
+++ app/components/Pagination/index.tsx
@@ -18,7 +18,7 @@ const Pagination = ({ currentPage, totalPages, onPageChange, key }: pagination) 
     }
 
     return (
-        <div className="flex items-center justify-between p-4 border-t border-blue-gray-50">
+        <div key={key} className="flex items-center justify-between p-4 border-t border-blue-gray-50">
             <button
                 className="select-none rounded-lg border border-gray-900 py-2 px-4 text-center align-middle font-sans text-xs font-bold uppercase text-gray-900 transition-all hover:opacity-75 focus:ring focus:ring-gray-300 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
                 type="button"
